31a5b50f3f37343b6d41b57dbc86d9ab6f85af80
Cooper Yang <ty1037@nyu.edu>
2020-11-06 01:02:15 -0500

    The Card Template(Scriptable Object)

---
+++ Assets/CardObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class CardObject : ScriptableObject
+{
+	public string cardName;
+
+	public Sprite background;
+
+	public Sprite artwork;
+
+	public Sprite icon;
+	public string description;
+	public int lifeSpan;
+	public int generateSpeed;
+}
