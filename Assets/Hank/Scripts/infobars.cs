799a425107ac9866a16ddd14b453bca3cf8f8536
Hank Feng <hankf007@gmail.com>
2020-11-07 19:16:56 -0500

    panel displays info now

---
+++ Assets/Hank/Scripts/infobars.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class infobars : MonoBehaviour
+{
+    public float religion;
+    public float politics;
+    public float culture;
+    public float tech;
+
+    public Image religionbar;
+    public Image politicsbar;
+    public Image culturebar;
+    public Image techbar;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        religionbar.fillAmount = religion / 100;
+        politicsbar.fillAmount = politics / 100;
+        culturebar.fillAmount = culture / 100;
+        techbar.fillAmount = tech / 100;
+    }
+}
