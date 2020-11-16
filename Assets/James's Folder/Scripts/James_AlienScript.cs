3f963712461c414e8301a5738acad3a21cacf4ed
DESKTOP-3C2MU8K\PAPAYA <jy2636@nyu.edu>
2020-11-17 01:46:48 +0800

    arrows show up when alien's numbers changes

--- Assets/James's Folder/Scripts/James_AlienScript.cs
+++ Assets/James's Folder/Scripts/James_AlienScript.cs
@@ -18,36 +18,36 @@ public class James_AlienScript : MonoBehaviour
 
 	private float radius = 0; // caculated with total influence
 
-	// these should be in GameManager
-    public float world_interval;
-	private float timer;
-	public GameObject politicDotPrefab;
-	public GameObject religionDotPrefab;
-	public GameObject cultureDotPrefab;
-	public GameObject techDotPrefab;
-	public GameObject africa;
-	public GameObject northAmerica;
-	public GameObject southAmerica;
-	public GameObject asia;
-	public GameObject europe;
-	public GameObject australia;
+	private float timer; // for generating dots
+	
+	public GameObject polVfxIcon;
+	public GameObject relVfxIcon;
+	public GameObject culVfxIcon;
+	public GameObject techVfxIcon;
 
-	public Camera cam;
 
 	private void Start()
 	{
-		timer = world_interval;
+		timer = GameManager.me.world_interval;
+		polVfxIcon.GetComponent<Image>().fillAmount = pol_Influence / 100;
+		culVfxIcon.GetComponent<Image>().fillAmount = cul_Influence / 100;
+		relVfxIcon.GetComponent<Image>().fillAmount = rel_Influence / 100;
+		techVfxIcon.GetComponent<Image>().fillAmount = tech_Influence / 100;
 	}
 
 	private void Update()
 	{
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			StartCoroutine(NumberChangedVFX(20, 0, 15, -30));
+		}
 		if (timer > 0)
 		{
 			timer -= Time.deltaTime;
 		}
 		else
 		{
-			timer = world_interval;
+			timer = GameManager.me.world_interval;
 			GenerateDots(gameObject);
 		}
 	}
@@ -65,7 +65,7 @@ public class James_AlienScript : MonoBehaviour
 		if (rnd <= aS.pol_Influence)
 		{
 			// pol dot
-			GameObject dot = Instantiate(politicDotPrefab);
+			GameObject dot = Instantiate(GameManager.me.politicDotPrefab);
 			SetParent(dot);
 			dot.transform.position = transform.position + Random.insideUnitSphere * radius;
 			dot.transform.position = new Vector3(dot.transform.position.x, dot.transform.position.y, 0);
@@ -73,7 +73,7 @@ public class James_AlienScript : MonoBehaviour
 		else if (rnd > aS.pol_Influence && rnd <= aS.pol_Influence + aS.rel_Influence)
 		{
 			// rel dot
-			GameObject dot = Instantiate(religionDotPrefab);
+			GameObject dot = Instantiate(GameManager.me.religionDotPrefab);
 			SetParent(dot);
 			dot.transform.position = transform.position + Random.insideUnitSphere * radius;
 			dot.transform.position = new Vector3(dot.transform.position.x, dot.transform.position.y, 0);
@@ -81,7 +81,7 @@ public class James_AlienScript : MonoBehaviour
 		else if (rnd > aS.pol_Influence + aS.rel_Influence && rnd <= influence_Total - aS.tech_Influence)
 		{
 			// cul dot
-			GameObject dot = Instantiate(cultureDotPrefab);
+			GameObject dot = Instantiate(GameManager.me.cultureDotPrefab);
 			SetParent(dot);
 			dot.transform.position = transform.position + Random.insideUnitSphere * radius;
 			dot.transform.position = new Vector3(dot.transform.position.x, dot.transform.position.y, 0);
@@ -89,7 +89,7 @@ public class James_AlienScript : MonoBehaviour
 		else if (rnd > influence_Total - aS.tech_Influence)
 		{
 			// tech dot
-			GameObject dot = Instantiate(techDotPrefab);
+			GameObject dot = Instantiate(GameManager.me.techDotPrefab);
 			SetParent(dot);
 			dot.transform.position = transform.position + Random.insideUnitSphere * radius;
 			dot.transform.position = new Vector3(dot.transform.position.x, dot.transform.position.y, 0);
@@ -105,35 +105,86 @@ public class James_AlienScript : MonoBehaviour
 		switch (myCon)
 		{
 			case Continent.africa:
-				dot.transform.parent = africa.transform;
-				africa.GetComponent<ContinentScript>().dots.Add(dot);
-				africa.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.africa.transform;
+				GameManager.me.africa.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.africa.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 			case Continent.northAmerica:
-				dot.transform.parent = northAmerica.transform;
-				northAmerica.GetComponent<ContinentScript>().dots.Add(dot);
-				northAmerica.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.northAmerica.transform;
+				GameManager.me.northAmerica.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.northAmerica.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 			case Continent.southAmerica:
-				dot.transform.parent = southAmerica.transform;
-				southAmerica.GetComponent<ContinentScript>().dots.Add(dot);
-				southAmerica.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.southAmerica.transform;
+				GameManager.me.southAmerica.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.southAmerica.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 			case Continent.asia:
-				dot.transform.parent = asia.transform;
-				asia.GetComponent<ContinentScript>().dots.Add(dot);
-				asia.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.asia.transform;
+				GameManager.me.asia.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.asia.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 			case Continent.europe:
-				dot.transform.parent = europe.transform;
-				europe.GetComponent<ContinentScript>().dots.Add(dot);
-				europe.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.europe.transform;
+				GameManager.me.europe.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.europe.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 			case Continent.australia:
-				dot.transform.parent = australia.transform;
-				australia.GetComponent<ContinentScript>().dots.Add(dot);
-				australia.GetComponent<ContinentScript>().CalculateIindividualDots();
+				dot.transform.parent = GameManager.me.australia.transform;
+				GameManager.me.australia.GetComponent<ContinentScript>().dots.Add(dot);
+				GameManager.me.australia.GetComponent<ContinentScript>().CalculateIindividualDots();
 				break;
 		}
+	}
+
+	public IEnumerator NumberChangedVFX(float pol, float cul, float rel, float tech) // input change amount
+	{
+		// show the corresponding icons
+		if (pol > 0)
+		{
+			polVfxIcon.transform.localScale = new Vector3(1, 1, 1);
+			polVfxIcon.SetActive(true);
+		}
+		else if (pol < 0)
+		{
+			polVfxIcon.transform.localScale = new Vector3(1, -1, 1);
+			polVfxIcon.SetActive(true);
+		}
+		if (cul > 0)
+		{
+			culVfxIcon.transform.localScale = new Vector3(1, 1, 1);
+			culVfxIcon.SetActive(true);
+		}
+		else if (cul < 0)
+		{
+			culVfxIcon.transform.localScale = new Vector3(1, -1, 1);
+			culVfxIcon.SetActive(true);
+		}
+		if (rel > 0)
+		{
+			relVfxIcon.transform.localScale = new Vector3(1, 1, 1);
+			relVfxIcon.SetActive(true);
+		}
+		else if (rel < 0)
+		{
+			relVfxIcon.transform.localScale = new Vector3(1, -1, 1);
+			relVfxIcon.SetActive(true);
+		}
+		if (tech > 0)
+		{
+			techVfxIcon.transform.localScale = new Vector3(1, 1, 1);
+			techVfxIcon.SetActive(true);
+		}
+		else if (tech < 0)
+		{
+			techVfxIcon.transform.localScale = new Vector3(1, -1, 1);
+			techVfxIcon.SetActive(true);
+		}
+		yield return new WaitForSeconds(1);
+		// hide all icons
+		polVfxIcon.SetActive(false);
+		culVfxIcon.SetActive(false);
+		relVfxIcon.SetActive(false);
+		techVfxIcon.SetActive(false);
 	}
 }
