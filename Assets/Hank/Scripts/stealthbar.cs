6c42d6ac0b44721d5d8fb3979fe2e6898140b122
Hank Feng <hankf007@gmail.com>
2020-11-18 20:35:14 -0500

    stealthbar

---
+++ Assets/Hank/Scripts/stealthbar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stealthbar : MonoBehaviour
+{
+    public Image fillImage;
+    public float charge;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        charge = gameObject.GetComponent<stealthlv>().stealthlev;
+        fillImage.fillAmount = charge / 100;
+
+
+    }
+}
---
+++ Assets/Hank/Scripts/stealthlv.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stealthlv : MonoBehaviour
+{
+    public int stealthlev;
+
+    public int smallInc;
+    public int midInc;
+    public int bigInc;
+    public int smallDec;
+    public int midDec;
+    public int bigDec;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stealthlev >= 100) //gameover
+        {
+
+        }
+    }
+
+    void SmallIncrease()
+    {
+        stealthlev += smallInc;
+    }
+    void MidIncrease()
+    {
+        stealthlev += midInc;
+    }
+    void BigIncrease()
+    {
+        stealthlev += bigInc;
+    }
+    void SmallDecrease()
+    {
+        if (stealthlev < smallDec)
+        {
+            stealthlev = 0;
+        }
+        else
+        {
+            stealthlev -= smallDec;
+        }
+        
+    }
+    void MidDecrease()
+    {
+        if (stealthlev < midDec)
+        {
+            stealthlev = 0;
+        }
+        else
+        {
+            stealthlev -= midDec;
+        }
+    }
+    void BigDecrease()
+    {
+        if (stealthlev < bigDec)
+        {
+            stealthlev = 0;
+        }
+        else
+        {
+            stealthlev -= bigDec;
+        }
+    }
+
+
+}
