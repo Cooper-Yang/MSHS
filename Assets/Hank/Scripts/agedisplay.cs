15cd86e1012b4a8b40e8a11f133d40be7393360e
Hank Feng <hankf007@gmail.com>
2020-11-23 21:15:38 -0500

    update info page

---
+++ Assets/Hank/Scripts/agedisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class agedisplay : MonoBehaviour
+{
+    public Text ageNum;
+    public int age;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        age = transform.parent.GetComponent<James_AlienScript>().lifeSpan;
+        ageNum.text = age.ToString();
+    }
+}
+    
