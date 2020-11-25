13a5436426ace420f91bc5c4b99155b8a57d3ad8
Ayesen <wx566@nyu.edu>
2020-11-24 21:53:13 -0500

    Delete Zone, News UI, Alien UI ready

--- Assets/Cooper/CardDelete.cs
+++ Assets/Cooper/CardDelete.cs
@@ -6,39 +6,53 @@ using UnityEngine.UI;
 public class CardDelete : MonoBehaviour
 {
 
-    Image deleteZone;
+
+    bool isOnImage;
+
     // Update is called once per frame
 
     private void Start()
     {
-        deleteZone = GameObject.Find("DeleteZone").GetComponent<Image>();
+
     }
 
     void Update()
     {
-        if(transform.position.y > 5)
+        if(isOnImage == true)
         {
-            deleteZone.color = new Color(deleteZone.color.r, deleteZone.color.g, deleteZone.color.b,0.5f);
-            //Debug.Log(transform.position.y); 
+            
             if (Input.GetMouseButtonUp(0))
             {
 
-                
-                deleteZone.color = new Color(deleteZone.color.r, deleteZone.color.g, deleteZone.color.b, 0);
                 TurnsManager._instance.nextTurn();
                 AudioManager._instance.DeletCard();
                 Destroy(gameObject);
             }
         }
-        else if (transform.position.y >-2)
-        {
-            deleteZone.color = new Color(deleteZone.color.r, deleteZone.color.g, deleteZone.color.b, 0);
-        }
 
 
 
 
 
-        //Debug.Log(transform.position.y);
     }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.tag == "DeleteZone")
+        {
+            isOnImage = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "DeleteZone")
+        {
+            isOnImage = false;
+        }
+    }
+
+
+
 }
