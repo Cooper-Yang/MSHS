68cf0460e423ea583b8e1e07fd693f6f99aa06ec
TakayaEnomoto <49789142+TakayaEnomoto@users.noreply.github.com>
2020-11-26 23:32:47 +0800

    + random values + random life span + adjusted icon bars

--- Assets/Cooper/AlienCardProcGen.cs
+++ Assets/Cooper/AlienCardProcGen.cs
@@ -28,75 +28,61 @@ public class AlienCardProcGen : MonoBehaviour
     public int lifeSpanNumber;
     public int genSpeed;
     public int discoverRate;
+
+    public int propertyMin;
+    public int propertyMax;
+    public int lifeMin;
+    public int lifeMax;
+    public int totalAbility;
+
+    public List<float> properties;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        // initialize list
+        for (int i = 0; i < 5; i++)
+        {
+            properties.Add(0);
+        }
         genSpeed = 3; //value unsure
         discoverRate = 2;//small
-
-
-        lifeSpanNumber = 9;
-        lifeSpan.text = "Life Span: " +lifeSpan;
-
-        int randomStat = Random.Range(0,4);         //0 = Poli, 1= culture, 2 = religion, 3= tech 
 
         //head Generate
         head.sprite = headSpr[Random.Range(0, headSpr.Length)];
         eyes.sprite = eyesSpr[Random.Range(0, eyesSpr.Length)];
         mouth.sprite = mouthSpr[Random.Range(0, mouthSpr.Length)];
 
-        Vector2 maxStat = new Vector2(165, 19);
-        Vector2 minStat = new Vector2(8, 19);
-        switch (randomStat)
+        SetValues();
+        lifeSpan.text = "Life Span: " + lifeSpanNumber;
+    }
+
+    private void SetValues()
+    {
+        // roll numbers
+        for (int i = 0; i < properties.Count; i++)
         {
-            
-            case 0:
-                polIcon.sizeDelta = maxStat;
-                culIcon.sizeDelta = minStat;
-                relIcon.sizeDelta = minStat;
-                techIcon.sizeDelta = minStat;
-                cul = 0;
-                rel = 0;
-                pol = 100;
-                tech =0;
-                break;
-            case 1:
-                polIcon.sizeDelta = minStat;
-                culIcon.sizeDelta = maxStat;
-                relIcon.sizeDelta = minStat;
-                techIcon.sizeDelta = minStat;
-                cul = 100;
-                rel = 0;
-                pol = 0;
-                tech = 0;
-                break;
-            case 2:
-                polIcon.sizeDelta = minStat;
-                culIcon.sizeDelta = minStat;
-                relIcon.sizeDelta = maxStat;
-                techIcon.sizeDelta = minStat;
-                cul = 0;
-                rel = 100;
-                pol = 0;
-                tech = 0;
-                break;
-            case 3:
-                polIcon.sizeDelta = minStat;
-                culIcon.sizeDelta = minStat;
-                relIcon.sizeDelta = minStat;
-                techIcon.sizeDelta = maxStat;
-                cul = 0;
-                rel = 0;
-                pol = 0;
-                tech = 100;
-                break;
+            properties[i] = Random.Range(0, totalAbility);
+            totalAbility -= (int)properties[i];
         }
-
-
-
-        
-
+        // shuffle numbers
+        for (int i = 0; i < properties.Count; i++)
+        {
+            float temp = properties[i];
+            int randomIndex = Random.Range(i, properties.Count);
+            properties[i] = properties[randomIndex];
+            properties[randomIndex] = temp;
+        }
+        // set numbers
+        cul = (int) (properties[0] / 100 * propertyMax);
+        rel = (int)(properties[1] / 100 * propertyMax);
+        pol = (int)(properties[2] / 100 * propertyMax);
+        tech = (int)(properties[3] / 100 * propertyMax);
+        lifeSpanNumber = 9 + (int)(properties[4] / 100 * lifeMax);
+        // set bar
+        culIcon.sizeDelta = new Vector2((100-8)*properties[0]/100, 19);
+        relIcon.sizeDelta = new Vector2((100-8)*properties[1]/100, 19);
+        polIcon.sizeDelta = new Vector2((100-8)*properties[2]/100, 19);
+        techIcon.sizeDelta = new Vector2((100-8)*properties[3]/100, 19);
     }
 }
